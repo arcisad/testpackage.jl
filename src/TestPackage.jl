13188bfd06efe9ae3e36005dd3e32d9409c96b28
Amir Rastar <amir.rastar@gmail.com>
2021-11-03 14:17:02 +1000

    new changes

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -7,7 +7,7 @@ using Random
 export generate_output
 
 function generate_output(a::Float64)::Float64
-    @info "generating results..."
+    @info "generating results...!"
     return norm([mean(rand(10)), a])
 end
 
